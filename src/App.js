4b09915f63e1e66afb08f7db2cd1b1bbf0723cf5
techmania-art <134226958+techmania-art@users.noreply.github.com>
2024-07-16 23:05:17 +0530

    Compelete

--- src/App.js
+++ src/App.js
@@ -1,19 +1,19 @@
 import logo from './logo.svg';
 import './App.css';
-import { Route, Router, Routes } from 'react-router-dom';
+import { BrowserRouter, Route, Router, Routes } from 'react-router-dom';
 import Details from './Component/Details';
 import Viewall from './Component/Viewall';
 import Search from './Component/Search';
 
 function App() {
   return (
-    <Router>
+    <BrowserRouter>
       <Routes>
-        <Route path='/' element={<Details/>}/>
-        <Route path='/search' element={<Search/>}/>
-        <Route path='/viewall' element={<Viewall/>}/>
+        <Route path='/' element={<Details />} />
+        <Route path='/search' element={<Search />} />
+        <Route path='/viewall' element={<Viewall />} />
       </Routes>
-    </Router>
+    </BrowserRouter>
   );
 }
 
